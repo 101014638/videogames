b5896a47c380ee8d4c2f2aea253d7c72c87db90a
Diana <dianacalpagalindres@gmail.com>
2024-08-23 21:31:33 -0500

    Actualizacion

---
+++ Scrist python/Ejemplo 1.py
@@ -0,0 +1,24 @@
+#este es un comentario de linea
+
+'''esto es un comentrario de bloque'''
+
+data = ''' esto es una variable string en bloque'''
+
+player = "Diana"
+life = 5
+
+n1 = 10
+n2 = 20
+suma = n1 + n2
+
+print(suma)
+print ("La suma es:",suma)
+print (f"La suma es: {suma}")
+print ("La suma es: " . n1 +n2)
+print ("La suma es: ", n1 +n2)
+print ("La suma es:", n1 +n2)
+
+
+
+
+
